44316424e1611a420a37820a396206b9d7e86d06
Robbyatul <robbyatul09@gmail.com>
2024-05-29 09:00:43 +0700

    Pekerjaan pertama CRUD

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    if(!isset($_session['email'])) {
+        header("Location: login.php");
+        exit;
+    }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+</head>
+<body>
+<div class="container w-75">
+<h1 class="My-4">Aplikasi Perpustakaan</h1>
+    
+    <a class="btn btn-primary" href="./buku.php">Lihat Daftar buku</a>
+    <a class="btn btn-success" href="./staff.php">Lihat Daftar buku</a>
+
+    <form class="mt-3" action="logout_proccess.php" method="POST">
+        <button class="btn btn-danger" type="submit">LOGOUT</button>
+    </form>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if (isset($_SESSION["email"])) {
+    header("Location: index.php");
+    exit(); // Tambahkan exit setelah header untuk menghentikan eksekusi
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    <div class="container"></div>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+<h2>Login Admin</h2>
+
+<form action="./login-process.php" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+<a class="btn btn-success" href="./register.php">Register</a>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+</body>
+</html>
